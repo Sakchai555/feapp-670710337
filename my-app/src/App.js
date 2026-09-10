e7543d1bd352367a6690b7c9acd5d80bab89dc05
Sakchai_337 <SAEAIAO_S2@silpakorn.edu>
2026-09-10 03:44:02 +0000

    ready

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -103,6 +103,10 @@ import About from './pages/About';
 import Navbar from './Component/Navbar';
 import Footer from './Component/Footer';
 import MovieDetail from './pages/MovieDetail';
+import NotFound from './pages/NotFound';
+
+function App() {
+  return (
     <div className="flex min-h-screen flex-col bg-slate-50">
       <Navbar />
       <main className="flex-1">
