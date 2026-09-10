78a4d3fef7684d834dc7c40a1078d494443c041c
Sakchai_337 <SAEAIAO_S2@silpakorn.edu>
2026-09-10 05:22:07 +0000

    New data & state

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,8 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import { movies } from '../Data/data'
+import MovieCard from '../Component/MovieCard'
+import MovieGallery from '../Component/MovieGallery';
+
 function Home() {
   return (
-    <div className="p-8 text-center">
-      <h1 className="text-3xl font-bold text-slate-800">🏠 หน้าแรก</h1>
-      <p className="mt-2 text-slate-500">ยินดีต้อนรับสู่ MovieHub</p>
+    <div className='mx-auto max-w-5xl p-8 text-center'>
+
+      <h1 className='mb-6 text-2xl font-bold text-slate-800'>
+        🏠 หน้าแรก
+      </h1>
+
+      <p className='mt-2 text-slate-500'>
+        ยินดีต้อนรับสู่ MovieHub
+      </p>
+
+      <h2 className='mt-8 text-2xl font-bold text-slate-800'>
+        หนังแนะนำ
+      </h2>
+
+      <div className='mx-auto mt-8 flex flex-wrap justify-center gap-6'>
+        {movies.map((m) => (
+          <MovieCard
+            key={m.id}
+            title={m.title}
+            year={m.year}
+          />
+        ))}
+      </div>
+        <h2 className='mt-8 text-2xl font-bold text-slate-800'>ดูหนังทีละเรื่อง</h2>
+        <div className='mx-auto mt-8 max-w-md'>
+          <MovieGallery/>
+        </div>
     </div>
   );
 }
