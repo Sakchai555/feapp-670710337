6926b3f4bfb5ae0d1c07aaf26df60efd9c370d15
Sakchai_337 <SAEAIAO_S2@silpakorn.edu>
2026-09-10 05:28:34 +0000

    ready to vercel

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import { movies } from '../Data/data'
 import MovieCard from '../Component/MovieCard'
 import MovieGallery from '../Component/MovieGallery';
